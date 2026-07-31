65e9893902c839cb7c03f26a98b897dc9e938f31
Murali2816 <testdevops2816@gmail.com>
2026-07-31 11:05:32 +0530

    Fix typo in greeting message in helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
 	public static void main(String[] args) {
-		System.out.println("Hello Murali, Good morning, welcome to the java session,compile done by jenkins");
+		System.out.println("Hello Murali, Good morning, welcome doneto the java session,compile done by jenkins");
 	}
 }
