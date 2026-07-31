b2974c2bd95ab4865aeb7d0bc3611ef63e17ddb7
Murali2816 <testdevops2816@gmail.com>
2026-07-31 10:21:18 +0530

    Update greeting message in helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
 	public static void main(String[] args) {
-		System.out.println("Hello Murali, Good morning, welcome to the Java session");
+		System.out.println("Hello Murali, Good morning, welcome to the Java session,we are running this file apartfrom the jenkins complie done by jenkins");
 	}
 }
